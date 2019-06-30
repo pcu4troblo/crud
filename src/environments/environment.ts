b5faea11bc27bc213d513825078c8ca4fc59b072
pcu4troblo <juanpablor56@gmail.com>
2019-06-29 21:27:34 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBX1NvqGezwOmsLrVetYV5h6YCoH9SWdBU",
+    authDomain: "crudangular-ad2c3.firebaseapp.com",
+    databaseURL: "https://crudangular-ad2c3.firebaseio.com",
+    projectId: "crudangular-ad2c3",
+    storageBucket: "",
+    messagingSenderId: "970152311868",
+    appId: "1:970152311868:web:00790a8c6a718315"
+  }
+};
